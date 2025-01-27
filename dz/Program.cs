b6ec241aa6e65e95cdd1f2f679ba65afee44f59c
Levikaci <121298029+Levikaci@users.noreply.github.com>
2025-01-27 18:44:49 +0300

    Добавьте файлы проекта.

---
+++ dz/Program.cs
@@ -0,0 +1,32 @@
+﻿public interface ISelectable
+{
+    void OnSelect();
+}
+
+public interface IUpdatable
+{
+    void Refresh();
+}
+
+public class Screen : ISelectable, IUpdatable
+{
+    public void OnSelect()
+    {
+        Console.WriteLine("Вывод метода OnSelect");
+    }
+
+    public void Refresh()
+    {
+        Console.WriteLine("Вывод метода Refresh");
+
+    }
+}
+public class Program
+{
+    public static void Main()
+    {
+        Screen screen = new Screen();
+        screen.OnSelect();
+        screen.Refresh();
+    }
+}
